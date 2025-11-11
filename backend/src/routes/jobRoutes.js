947b19f9c7c1950e3c8cbdccf04a8fdf683b57b2
PCoderHub <poojapbalachandran@gmail.com>
2025-11-11 14:19:39 +0000

    minor change in jobRoutes file

--- backend/src/routes/jobRoutes.js
+++ backend/src/routes/jobRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { route } = require("./clientRoutes");
 const router = express.Router();
 
 router.post("/");           //create job, client only
@@ -10,3 +9,5 @@ router.delete("/:id");      //delete job by id, client, admin only
 router.post("/:id/apply");  //apply to job by id, freelancer only
 router.get("/:id/proposals");       //get proposals for job by id, client only
 router.put(":id/assign");  //assign freelancer to job by id, client only
+
+module.exports = router;
