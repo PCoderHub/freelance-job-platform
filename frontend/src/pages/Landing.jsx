770f6e72d1ae6601d409ca0fee55e5acd16e965d
PCoderHub <poojapbalachandran@gmail.com>
2025-11-19 00:22:15 +0000

    static content for landing page added as per design

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Landing() {
-  return <div></div>;
+  return (
+    <div className="container mx-auto px-4 my-15">
+      <section className="bg-indigo-300 text-center mb-15 text-white rounded-sm">
+        <h2 className="text-2xl font-bold mb-4 p-30">
+          Become a part of our freelance community
+        </h2>
+      </section>
+      <section className="bg-indigo-600 text-center mb-15 text-white rounded-sm">
+        <h2 className="text-2xl font-bold mb-4 p-30">
+          Let our freelancers help you
+        </h2>
+      </section>
+      <section className="bg-indigo-300 text-center text-white rounded-sm flex justify-around items-center p-10">
+        <div className="bg-white text-black rounded-sm w-1/5 h-60 flex justify-center items-center">
+          Software Development
+        </div>
+        <div className="bg-white text-black rounded-sm w-1/5 h-60 flex justify-center items-center">
+          Content Writing
+        </div>
+        <div className="bg-white text-black rounded-sm w-1/5 h-60 flex justify-center items-center">
+          Data Entry
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Landing;
