2998e40fe7664adb2b4067272004cc8d5b08f79f
PCoderHub <poojapbalachandran@gmail.com>
2026-01-05 15:45:26 +0530

    left image of login and register border fixed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -51,7 +51,7 @@ function Login() {
           <img
             src="https://images.unsplash.com/photo-1620344527458-28900b32d695?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
-            className="object-cover h-full rounded-sm"
+            className="object-cover h-full rounded-t-sm lg:rounded-l-sm lg:rounded-t-none"
           />
         </div>
         <div className="w-full lg:w-1/2 text-center py-20">
